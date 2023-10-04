5c9feca7d696ae7957ff3a15ea8a730db0a48778
Aldonazi <aldo.04070@gmail.com>
2023-10-04 11:45:37 -0600

    practica06

---
+++ practica06.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById("envio")
+        formulario.addEventListener("click",evento=>{
+            evento.preventDefault()
+            let u=document.getElementById("user").value
+            let p=document.getElementById("password").value
+            alert(u+p)
+        })
+        
